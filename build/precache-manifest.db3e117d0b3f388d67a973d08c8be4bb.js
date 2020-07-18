6296d3060a0958cae930cc6c3cadebe147917a75
hugo costa <gohucosta23@hotmail.com>
2020-07-18 18:20:59 -0400

    deploying to heroku

---
+++ build/precache-manifest.db3e117d0b3f388d67a973d08c8be4bb.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96f7040b43c12b00be82632b198a0f0d",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "c76786c2783598de8c51",
+    "url": "/react-portfolio/static/css/2.e303e1ad.chunk.css"
+  },
+  {
+    "revision": "ec5420761413ce638066",
+    "url": "/react-portfolio/static/css/main.feea7a87.chunk.css"
+  },
+  {
+    "revision": "c76786c2783598de8c51",
+    "url": "/react-portfolio/static/js/2.161c73bd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-portfolio/static/js/2.161c73bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec5420761413ce638066",
+    "url": "/react-portfolio/static/js/main.dcf5e8cf.chunk.js"
+  },
+  {
+    "revision": "aff33cef9dbb8787d6b3",
+    "url": "/react-portfolio/static/js/runtime-main.213b1880.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/react-portfolio/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/react-portfolio/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/react-portfolio/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/react-portfolio/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/react-portfolio/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "0c957bf0db88d8b1eed7647d043c0780",
+    "url": "/react-portfolio/static/media/burgerGif.0c957bf0.gif"
+  },
+  {
+    "revision": "d4dde8933a2c7951530dcd3cd0525986",
+    "url": "/react-portfolio/static/media/dayPlanner.d4dde893.png"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/react-portfolio/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "3db3464302a9743e4c839aa3b508f850",
+    "url": "/react-portfolio/static/media/goodtogo.3db34643.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/react-portfolio/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/react-portfolio/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/react-portfolio/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/react-portfolio/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/react-portfolio/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "4db6ca87ea1cd3f39ce28e8648cd6d3e",
+    "url": "/react-portfolio/static/media/notetaker1.4db6ca87.png"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/react-portfolio/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/react-portfolio/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/react-portfolio/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/react-portfolio/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/react-portfolio/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "78b4ce4f9d673f74ee54f97978172f21",
+    "url": "/react-portfolio/static/media/p1.78b4ce4f.png"
+  },
+  {
+    "revision": "b12228ea076af998d171bca5d6d45926",
+    "url": "/react-portfolio/static/media/photo.b12228ea.jpeg"
+  },
+  {
+    "revision": "9ee71b7f1d9ee28f18a774d93e9c5213",
+    "url": "/react-portfolio/static/media/resume.9ee71b7f.pdf"
+  },
+  {
+    "revision": "67df71abbefd2e1bc43334c235b8e3fa",
+    "url": "/react-portfolio/static/media/weatherDashboard.67df71ab.png"
+  }
+]);
